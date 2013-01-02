b5a62c68a828fffac7d05cdb18e33581984870f4
berry120@gmail.com <berry120@gmail.com@5ea63541-afa0-a802-4c4f-c349d9dc4b9c>
2013-01-02 00:53:16 +0000

    Add all other cache type images

--- GeoPoke/src/org/geopoke/CacheType.java
+++ GeoPoke/src/org/geopoke/CacheType.java
@@ -11,17 +11,17 @@ public enum CacheType {
     TRADITIONAL(true, new Image("file:img/trad.gif")),
     MULTI(false, new Image("file:img/multi.gif")),
     MYSTERY(false, new Image("file:img/mystery.gif")),
-    HYBRID(true),
-    VIRTUAL(true),
-    EVENT(true),
-    APE(true),
-    WEBCAM(true),
-    REVERSE(true),
-    CITO(true),
-    EARTH(true),
-    MEGAEVENT(true),
-    WHERIGO(true),
-    HQ(true);
+    HYBRID(true, new Image("file:img/hybrid.gif")),
+    VIRTUAL(true, new Image("file:img/virtual.gif")),
+    EVENT(true, new Image("file:img/event.gif")),
+    APE(true, new Image("file:img/ape.gif")),
+    WEBCAM(true, new Image("file:img/webcam.gif")),
+    REVERSE(true, new Image("file:img/locationless.gif")),
+    CITO(true, new Image("file:img/cito.gif")),
+    EARTH(true, new Image("file:img/earth.gif")),
+    MEGAEVENT(true, new Image("file:img/megaevent.gif")),
+    WHERIGO(true, new Image("file:img/wherigo.gif")),
+    HQ(true, new Image("file:img/hq.gif"));
     
     private Image defaultImg = new Image("file:img/trad.gif");
     private boolean initialAccurateCoords;
