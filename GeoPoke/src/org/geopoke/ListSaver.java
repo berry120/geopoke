398600b073331135adc0cc5808f160a9459e6b90
berry120@gmail.com <berry120@gmail.com@5ea63541-afa0-a802-4c4f-c349d9dc4b9c>
2013-01-04 02:40:52 +0000

    Added generic type to getCaches() method

--- GeoPoke/src/org/geopoke/ListSaver.java
+++ GeoPoke/src/org/geopoke/ListSaver.java
@@ -36,7 +36,7 @@ public class ListSaver {
     private static final Logger LOGGER = Logger.getLogger(ListSaver.class.getName());
 
     public void getCaches(final ScrapeSession session, final File file,
-            final ProgressUpdator updator, final Callback finished) {
+            final ProgressUpdator updator, final Callback<Geocache[]> finished) {
         Thread t = new Thread() {
             public void run() {
                 try {
